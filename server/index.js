6d97c8f497ccccc9c8f4cb2afb62e04e0135f15f
Swathika-P <swathika.p2021csec@sece.ac.in>
2024-08-14 20:11:15 +0530

    fix: update CORS origin and API routes for deployment

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-mate-b00m.onrender.com/",
     credentials: true,
   },
 });
